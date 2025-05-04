cf3f44f6b4b031fb80f2986c7bd0524594259de8
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-05 00:23:24 +0600

    project setup with talwind,daisyUI and basic route setup with react router

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <header>
+                <Navbar/>
+            </header>
+            <main>
+                <Outlet/>
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
+        </div>
+    );
+};
+
+export default Root;
